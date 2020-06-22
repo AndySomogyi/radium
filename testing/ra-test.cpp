833ff2d2e373614d4cc1715c4c493b917b6aa156
Andy Somogyi <andy.somogyi@gmail.com>
2020-06-21 20:17:44 -0400

    uddates

--- testing/ra-test.cpp
+++ testing/ra-test.cpp
@@ -10,6 +10,8 @@
 
 void cairo_test(RaApplication *app);
 
+void foo();
+
 int main(int argc, const char** argv) {
   const char* cairo_ver = cairo_version_string ();
   std::cout << "cairo version: " << cairo_ver << std::endl;
@@ -20,6 +22,8 @@ int main(int argc, const char** argv) {
 
   RaApplication_Run(app);
 
+  foo();
+
   return 0;
 }
 
@@ -28,9 +32,9 @@ void cairo_test(RaApplication *app) {
     cairo_surface_t *surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 900, 600);
 
     cairo_t *cr = cairo_create(surface);
-    
+
     cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 1.0);
-    
+
     cairo_set_source_rgb (cr, 1, 1, 1);
     cairo_rectangle (cr, 0.0, 0.0, 0.5, 0.5);
     cairo_fill (cr);
@@ -74,27 +78,8 @@ void cairo_test(RaApplication *app) {
 
 void foo() {
 
-    /*
-    cairo_surface_t *surface; cairo_t *cr;
+    CCompartment_t *c = CCompartment_create();
 
-    // initsurface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
 
-    cr = cairo_create(surface);
-
-    // in main loopwhile(...)
-    {
-        render_something_to_surface();
-        // get a pointer to the ARGB data
-        unsigned char * data = cairo_image_surface_get_data(surface);
-
-        // use the data
-        glBindTexture(GL_TEXTURE_2D,tex);
-
-        glTexSubImage2D(GL_TEXTURE_2D, 0, 0,0, width, height,GL_RGBA, GL_UNSIGNED_BYTE, data);}
-
-       // clean upcairo_destroy(cr);cairo_surface_destroy(surface);
-    }
-
-    */
 }
 
